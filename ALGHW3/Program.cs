ed4cd2465a19cdc2dc0298a9410c2c7265a024a4
roma_outspin <roma_outspin@mail.ru>
2021-06-17 20:09:34 +0300

    Initial commit

---
+++ ALGHW3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ALGHW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
