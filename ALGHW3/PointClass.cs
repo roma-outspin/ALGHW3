ca0c50f7ec613bbf79f4cf063f22b55a1d338c6a
roma_outspin <roma_outspin@mail.ru>
2021-06-17 22:14:37 +0300

    ready to check

---
+++ ALGHW3/PointClass.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ALGHW3
+{
+    public class PointClass
+    {
+        public int X;
+        public int Y;
+
+
+    }
+}
--- ALGHW3/Program.cs
+++ ALGHW3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
 
 namespace ALGHW3
 {
@@ -6,7 +6,34 @@ namespace ALGHW3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         }
     }
+
+    //public class BechmarkClass
+    //{
+    //    public int SumValueType(int value)
+    //    {
+    //        return 9 + value;
+    //    }
+
+    //    public int SumRefType(object value)
+    //    {
+    //        return 9 + (int)value;
+    //    }
+
+    //    [Benchmark]
+    //    public void TestSum()
+    //    {
+    //        SumValueType(99);
+    //    }
+
+    //    [Benchmark]
+    //    public void TestSumBoxing()
+    //    {
+    //        object x = 99;
+    //        SumRefType(x);
+    //    }
+
+    //}
 }
